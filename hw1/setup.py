d43704dffb9f04cd1d7dd122900b096f7704e160
OlesiaSub <lesya.sub@mail.ru>
2022-02-13 01:00:21 +0300

    Fix setup

--- hw1/setup.py
+++ hw1/setup.py
@@ -2,10 +2,11 @@ import setuptools
 
 setuptools.setup(
     name="sastvisualizer",
-    version="1.0.7",
+    version="1.1.0",
     author="OlesiaSub",
     author_email="lesya.sub@mail.ru",
     description="Python course hw1. AST visualizer.",
+    packages=['sastvisualizer'],
     url="https://github.com/OlesiaSub/hse-python-2022/tree/master/hw1",
     install_requires=["networkx", "pydot"]
 )
