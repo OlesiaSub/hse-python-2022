e126e79aa071706263b4a3b1eaa1f599882965f9
OlesiaSub <lesya.sub@mail.ru>
2022-02-13 01:22:14 +0300

    Fix structure

--- hw1/sastvisualizer/main.py
+++ hw1/sastvisualizer/main.py
@@ -16,11 +16,15 @@ def fib_seq(n: int):
         nxt = prev + cur
 
 
-if __name__ == '__main__':
+def generate_ast(path):
     visitor = MyASTVisitor()
     visitor.visit(ast.parse(inspect.getsource(fib_seq)))
     G = visitor.ast_graph
     pos = nx.spring_layout(G)
     nx.draw(G, pos, with_labels=True)
     p = nx.drawing.nx_pydot.to_pydot(G)
-    p.write_png('artifacts/fib_ast.png')
+    p.write_png(path)
+
+
+if __name__ == '__main__':
+    generate_ast('fib_ast.png')
--- hw1/setup.py
+++ hw1/setup.py
@@ -2,12 +2,12 @@ import setuptools
 
 setuptools.setup(
     name="sastvisualizer",
-    version="1.1.0",
+    version="1.1.1",
     author="OlesiaSub",
     author_email="lesya.sub@mail.ru",
     description="Python course hw1. AST visualizer.",
     packages=['sastvisualizer'],
     url="https://github.com/OlesiaSub/hse-python-2022/tree/master/hw1",
-    install_requires=["networkx", "pydot"]
+    install_requires=["networkx", "pydot", "numpy", "matplotlib"]
 )
 
