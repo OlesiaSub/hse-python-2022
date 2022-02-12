ddcb979573387ea9d51e98f9c07d235e979b7bd0
OlesiaSub <lesya.sub@mail.ru>
2022-02-13 00:24:45 +0300

    Change structure

---
+++ hw1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="sastvisualizer",
+    version="1.0.4",
+    author="OlesiaSub",
+    author_email="lesya.sub@mail.ru",
+    description="Python course hw1. AST visualizer.",
+    url="https://github.com/OlesiaSub/hse-python-2022/tree/master/hw1",
+    install_requires=["networkx", "pydot"]
+)
+
